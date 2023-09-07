62109bee5e9363e206d0446b9880c0edf7b8e00d
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2023-09-07 14:54:44 +0800

    Adjusted Cutscene

--- Assets/Scripts/MainMenuScripts/MainMenu.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void QuitGame()
