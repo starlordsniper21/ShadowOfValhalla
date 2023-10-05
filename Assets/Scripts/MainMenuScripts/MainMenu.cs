f43666cb2728ff7c46b45373e34972535d6ecc30
charlie141 <columbrescharlie@gmail.com>
2023-10-05 14:00:41 +0800

    UPDATE MAIN MENU UI AND DESIGN

--- Assets/Scripts/MainMenuScripts/MainMenu.cs
+++ Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -5,9 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    // This function will be called when the "Chapter 1" button is clicked.
+    public void PlayChapter1()
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync("CHAPTER 1");
+    }
+
+    // This function will be called when the "Chapter 2" button is clicked.
+    public void PlayChapter2()
+    {
+        SceneManager.LoadSceneAsync("CHAPTER 2");
+    }
+
+    // This function will be called when the "Chapter 3" button is clicked.
+    public void PlayChapter3()
+    {
+        SceneManager.LoadSceneAsync("CHAPTER 3");
+    }
+
+    // This function will be called when the "Chapter 4" button is clicked.
+    public void PlayChapter4()
+    {
+        SceneManager.LoadSceneAsync("CHAPTER 4");
+    }
+
+    // This function will be called when the "Chapter 5" button is clicked.
+    public void PlayChapter5()
+    {
+        SceneManager.LoadSceneAsync("CHAPTER 5");
     }
 
     public void QuitGame()
