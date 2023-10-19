fecc5fdb23cff44b6fbb5f056d9a9f62c0a067dc
charlie141 <columbrescharlie@gmail.com>
2023-10-19 21:34:18 +0800

    fixed a few bugs and added loading screen on chapter 1

--- Assets/Scripts/transform location.cs
+++ Assets/Scripts/transform location.cs
@@ -1,17 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeleportController : MonoBehaviour
 {
-    public Transform destination; 
+    public Transform destination;
+    public Canvas blackScreenCanvas; // Reference to the UI Canvas
+    public float pauseTime = 5.0f; // Time to pause in seconds
+
+    private bool isTeleporting = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isTeleporting)
         {
-            // Teleport the player to the destination.
-            other.transform.position = destination.position;
+            isTeleporting = true;
+
+            // Activate the black screen panel.
+            blackScreenCanvas.gameObject.SetActive(true);
+
+            // Pause the game by setting the time scale to 0.
+            Time.timeScale = 0;
+
+            // Start a coroutine to resume the game and teleport the player.
+            StartCoroutine(ResumeAndTeleport(other.transform));
         }
+    }
+
+    private IEnumerator ResumeAndTeleport(Transform playerTransform)
+    {
+        // Wait for the specified pause time.
+        yield return new WaitForSecondsRealtime(pauseTime);
+
+        // Deactivate the black screen panel.
+        blackScreenCanvas.gameObject.SetActive(false);
+
+        // Resume the game by setting the time scale back to 1.
+        Time.timeScale = 1;
+
+        // Teleport the player to the destination.
+        playerTransform.position = destination.position;
+
+        // Reset the teleporting flag.
+        isTeleporting = false;
     }
 }
