edcedf6db3136a2dfc06efe89ce5e6b3f03c004e
charlie141 <columbrescharlie@gmail.com>
2024-01-29 16:35:41 +0800

    Inventory Update ( Implemented weapon swap button in inventory)

--- Assets/Scripts/swap weapons/weapon swapt.cs
+++ Assets/Scripts/swap weapons/weapon swapt.cs
@@ -5,13 +5,39 @@ public class ButtonSwapper : MonoBehaviour
 {
     public Button swordButton;
     public Button bowButton;
+    public Button swapButton;
+    public Button backToSwapButton;
 
     private bool isSwordActive = true;
+
+    void Start()
+    {
+        InitializeButtonsVisibility();
+    }
+
+    void InitializeButtonsVisibility()
+    {
+        swordButton.gameObject.SetActive(isSwordActive);
+        bowButton.gameObject.SetActive(!isSwordActive);
+        swapButton.gameObject.SetActive(true);
+        backToSwapButton.gameObject.SetActive(false);
+    }
 
     public void SwapButtons()
     {
         isSwordActive = !isSwordActive;
         swordButton.gameObject.SetActive(isSwordActive);
         bowButton.gameObject.SetActive(!isSwordActive);
+        swapButton.gameObject.SetActive(false);
+        backToSwapButton.gameObject.SetActive(true);
+    }
+
+    public void BackToSwap()
+    {
+        isSwordActive = !isSwordActive;
+        swordButton.gameObject.SetActive(isSwordActive);
+        bowButton.gameObject.SetActive(!isSwordActive);
+        swapButton.gameObject.SetActive(true);
+        backToSwapButton.gameObject.SetActive(false);
     }
 }
