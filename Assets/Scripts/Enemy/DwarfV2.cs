96faaa140d36a51f87f24a19a3da4fecf9b570d2
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2024-03-07 10:25:19 +0800

    07/03/2024
    -Added Armored Mage Animations
    -Removed Dwarf Variant And Dwarf Mob Walk ( removed because of asset not being centered and can't remove scripts ) I Placed Dwarf Enemy
    -New Dwarf Enemy Scripts

---
+++ Assets/Scripts/Enemy/DwarfV2.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwarfV2 : MonoBehaviour
+{
+    public float detectionRadius = 5f;
+    public float preparationRadius = 4f;
+    public float attackRadius = 2f;
+    public float prepareTime = 5f;
+    public float attackInterval = 5f;
+    public float movementSpeed = 2f;
+    public float disableRadius = 8f;
+    public Animator animator;
+    public GameObject DwarfAttackArea;
+    private Transform player;
+    private bool playerInRange = false;
+    private bool preparing = false;
+    private float prepareTimer = 0f;
+    private float attackTimer = 0f;
+    bool isVikingAttackAreaActive = false;
+    float timeElapsedAfterPreparation = 0f;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        DwarfAttackArea.SetActive(false);
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        animator.SetBool("DwarfI", true);
+
+        if (distanceToPlayer <= detectionRadius)
+        {
+            playerInRange = true;
+            if (!preparing)
+            {
+                Vector2 direction = (player.position - transform.position).normalized;
+                transform.Translate(direction * movementSpeed * Time.deltaTime);
+
+                // Flipping Logic nung enemy
+
+                if (direction.x > 0 && !facingRight)
+                {
+                    Flip();
+                }
+                else if (direction.x < 0 && facingRight)
+                {
+                    Flip();
+                }
+
+                // run animation 
+                animator.SetBool("IsRunningDwarf", true);
+            }
+            if (distanceToPlayer <= preparationRadius && !preparing)
+            {
+                // prepare attack animation
+                //animator.SetTrigger("VikingR");
+                preparing = true;
+                prepareTimer = 0f;
+                Debug.Log("Enemy starts preparing for attack.");
+            }
+
+            if (preparing)
+            {
+                prepareTimer += Time.deltaTime;
+                if (prepareTimer >= prepareTime)
+                {
+                    preparing = false;
+                    Debug.Log("Preparation for attack complete.");
+
+                    // Trigger attack animation
+                    animator.SetTrigger("Attack");
+                }
+            }
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("DwarfAttack") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            {
+
+                DwarfAttackArea.SetActive(true);
+                isVikingAttackAreaActive = true;
+            }
+            else
+            {
+                DwarfAttackArea.SetActive(false);
+                isVikingAttackAreaActive = false;
+            }
+
+            if (distanceToPlayer <= attackRadius)
+            {
+                // wala lang
+            }
+        }
+        else
+        {
+            playerInRange = false;
+            prepareTimer = 0f;
+            attackTimer = 0f;
+            preparing = false;
+
+
+            DwarfAttackArea.SetActive(false);
+            isVikingAttackAreaActive = false;
+
+
+            animator.SetBool("IsRunningDwarf", false);
+        }
+    }
+
+    private void Flip()
+    {
+
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, preparationRadius);
+
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireSphere(transform.position, disableRadius);
+    }
+}
