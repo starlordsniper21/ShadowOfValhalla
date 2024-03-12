3aed8b83cd459d1f78e9b390e79972cbb6cc1931
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2024-03-12 11:13:37 +0800

    Hotfix

--- Assets/VaseObject.cs
+++ Assets/VaseObject.cs
@@ -7,7 +7,7 @@ public class BreakableObject : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("AttackArea"))
         {
             BreakObject();
         }
