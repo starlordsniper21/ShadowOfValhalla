3b9f5cce0ff7ce4a7e9676d2c04cdaba38a71152
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2024-03-17 13:15:47 +0800

    Joystick #1

--- Assets/Scripts/INTERACTION BUTTON/MovementJoystick.cs
+++ Assets/Scripts/INTERACTION BUTTON/MovementJoystick.cs
@@ -1,65 +1,53 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MovementJoystick : MonoBehaviour
+public class MovementJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
     private Animator animator;
     public GameObject joystick;
     public GameObject joystickBG;
     public Vector2 joystickVec;
-    private Vector2 joystickTouchPos;
     private Vector2 joystickOriginalPos;
     private float joystickRadius;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        
-    }
-
-
-  
-    void Start()
-    {
         joystickOriginalPos = joystickBG.transform.position;
         joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 4;
     }
 
-    public void PointerDown()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        joystick.transform.position = Input.mousePosition;
-        joystickBG.transform.position = Input.mousePosition;
-        joystickTouchPos = Input.mousePosition;
+        Vector2 localPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBG.GetComponent<RectTransform>(), eventData.position, null, out localPos);
+        joystick.transform.localPosition = localPos;
+        joystickBG.transform.position = eventData.position;
+        joystickVec = Vector2.zero;
     }
 
-    public void Drag(BaseEventData baseEventData)
+    public void OnDrag(PointerEventData eventData)
     {
-        PointerEventData pointerEventData = baseEventData as PointerEventData;
-        Vector2 dragPos = pointerEventData.position;
-        joystickVec = (dragPos - joystickTouchPos).normalized;
+        Vector2 localPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBG.GetComponent<RectTransform>(), eventData.position, null, out localPos);
+        joystickVec = (localPos - Vector2.zero).normalized;
 
-        float joystickDist = Vector2.Distance(dragPos, joystickTouchPos);
+        float joystickDist = Vector2.Distance(localPos, Vector2.zero);
 
         if (joystickDist < joystickRadius)
         {
-            joystick.transform.position = joystickTouchPos + joystickVec * joystickDist;
+            joystick.transform.localPosition = localPos;
         }
-
         else
         {
-            joystick.transform.position = joystickTouchPos + joystickVec * joystickRadius;
+            joystick.transform.localPosition = Vector2.ClampMagnitude(joystickVec * joystickRadius, joystickRadius);
         }
     }
 
-    public void PointerUp()
+    public void OnPointerUp(PointerEventData eventData)
     {
         joystickVec = Vector2.zero;
-        joystick.transform.position = joystickOriginalPos;
+        joystick.transform.localPosition = Vector2.zero;
         joystickBG.transform.position = joystickOriginalPos;
     }
 }
